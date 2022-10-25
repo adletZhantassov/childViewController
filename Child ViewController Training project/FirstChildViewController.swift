6160fec665169185b2e7b2f94a88d49a66f5011e
Adlet <adlet_1996@mail.ru>
2022-10-25 21:19:39 +0600

    main project

---
+++ Child ViewController Training project/FirstChildViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FirstChildViewController.swift
+//  Child ViewController Training project
+//
+//  Created by Adlet Zhantassov on 25.10.2022.
+//
+
+import UIKit
+
+class FirstChildViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+}
---
+++ Child ViewController Training project/SecondChildViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondChildViewController.swift
+//  Child ViewController Training project
+//
+//  Created by Adlet Zhantassov on 25.10.2022.
+//
+
+import UIKit
+
+class SecondChildViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .blue
+    }
+    
+
+
+}
